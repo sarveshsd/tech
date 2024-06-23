1f62be1f0db87b18dd2b765bd98f3af13dcb6696
sarveshsd <sarveshdharmadhikari4@gmail.com>
2024-06-23 18:30:54 +0530

    added deploy script

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -12,11 +12,6 @@ kubectl port-forward --address 0.0.0.0 svc/frontend 3000:80 &
 kubectl port-forward --address 0.0.0.0 svc/backend 8080:8080 &
 kubectl port-forward --address 0.0.0.0 svc/postgres 5432:5432 &
 
-# Wait for pods to be running before proceeding
-while ! pod_is_running frontend YOUR_NAMESPACE && ! pod_is_running backend YOUR_NAMESPACE && ! pod_is_running postgres YOUR_NAMESPACE; do
-  echo "Waiting for pods to be running..."
-  sleep 10
-done
 
 # Once all pods are running, proceed with port forwarding
 wait
