3d7f1f3be49e05b6592b9655a6bdbe84e9448907
sarveshsd <sarveshdharmadhikari4@gmail.com>
2024-06-23 19:47:17 +0530

    added deploy script

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-wait 60s
-
 kubectl port-forward --address 0.0.0.0 svc/frontend 3000:80 &
 kubectl port-forward --address 0.0.0.0 svc/backend 8080:8080 &
 kubectl port-forward --address 0.0.0.0 svc/postgres 5432:5432 &
